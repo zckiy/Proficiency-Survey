b0edcfd7c202cd096e5e0c08920ad85205789c96
zckiy <zacky20okto@gmail.com>
2024-10-03 21:31:43 +0700

    nabar

---
+++ src/pages/Responden/LandingPage.js
@@ -0,0 +1,11 @@
+import '@fontsource/roboto/400.css';
+
+function LandingPage() {
+  return (
+    <div className="App">
+        <p>Landing Page</p>
+    </div>
+  );
+}
+
+export default LandingPage;
