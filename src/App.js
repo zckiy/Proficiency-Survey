8bb938b999d997a104dabcd028e9463f0e5288e7
zahrul0909 <zahrulromadhon28@gmail.com>
2024-10-04 23:34:36 +0700

    Meembuat Tampilan Login Admin dan Pengisian Data Diri Pengguna

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import LandingPage from './pages/Responden/LandingPage';
+// import Navbar from './components/Navbar';
+// import LandingPage from './pages/Responden/LandingPage';
+// import IsiDataIndustri from './pages/Responden/IsiDataIndustri';
+// import IsiDataAlumni from './pages/Responden/IsiDataAlumni';
+// import IsiDataDosen from './pages/Responden/IsiDataDosen';
+
+import LoginAdmin from './admin/LoginAdmin'; 
+
+
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
-        <Route path="/*" element={<LandingPage />} />
+        {/* <Route path="/*" element={<LandingPage />} /> */}
+        {/* <Route path="/" element={<IsiDataIndustri />} /> */}
+        {/* <Route path="/" element={<IsiDataAlumni />} /> */}
+        {/* <Route path="/" element={<IsiDataDosen />} /> */}
+
+        <Route path="/" element={<LoginAdmin />} />
       </Routes>
     </Router>
   );
